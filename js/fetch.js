e60fcf451dfad33daa1781997141583bb3c6d40b
Afi Adhikari <afiadhikari.01@gmail.com>
2018-06-24 16:45:26 +0530

    Give UI to Login page. --Move sent messages to new page. --Show sent messages on login --Store id in localStorage on successful login --Add logout button

--- js/fetch.js
+++ js/fetch.js
@@ -1,31 +1,30 @@
 function displayMessage (){
-    		
-    		var id = $("#id").val(); 
+	var id = localStorage.id; 
 
-    		$.ajax({
-    			 type: "POST",
-			      url: "http://bling-test.000webhostapp.com/get-sent-msg-data.php",
-			      data: {
-			        fac_id: id
-			      },
-			      success: function (data){
-			      	var res = JSON.parse(data);
-			      	console.log(res);
-			      	
-			      	var html = "";
-			      	for(var a = 0; a<res.length; a++ ){
-			      		var date = res[a].date;
-			      		var subject = res[a].subject;
-			      		var message = res[a].message;
+	$.ajax({
+		type: "POST",
+		url: "http://bling-test.000webhostapp.com/get-sent-msg-data.php",
+		data: {
+			fac_id: id
+		},
+		success: function (data){
+			var res = JSON.parse(data);
+			console.log(res);
+			
+			var html = "";
+			for(var a = 0; a<res.length; a++ ){
+				var date = res[a].date;
+				var subject = res[a].subject;
+				var message = res[a].message;
 
-			      		html += date + subject + message;
-			      		html += "<br>"
-			      	}
-			      	document.getElementById("show").innerHTML = html;
-			      },
+				html += date + subject + message;
+				html += "<br>"
+			}
+			document.getElementById("show").innerHTML = html;
+		},
 
-			      error: function(error){
-			        console.log(error);
-			      }
-   		});
-   	}
+		error: function(error){
+			console.log(error);
+		}
+	});
+}
--- js/main.js
+++ js/main.js
@@ -1,35 +1,41 @@
 function login() {
+  $("#result").text("Inside send");
 
-    $("#result").text("Inside send");
+  var id = $("#id").val(); 
+  var pass = $("#pass").val();
 
-    var id = $("#id").val(); 
-    var pass = $("#pass").val();
+  $.ajax({
+    type: "POST",
+    url: "http://bling-test.000webhostapp.com/login.php",
+    data: {
+      id: id,
+      pass: pass
+    },
+    
+    success: function(response) {
+      var res = JSON.parse(response);
 
-    $.ajax({
-      type: "POST",
-      url: "http://bling-test.000webhostapp.com/login.php",
-      data: {
-        id: id,
-        pass: pass
-      },
-      
-      success: function(response) {
-        var res = JSON.parse(response);
-
-        if (res.res_type == "failed") {
-          $("#result").text("Incorrect ID/Password");
-          console.log("Incorrect ID/Password");
-        }
-
-        else if(res.res_type == "faculty") {
-          $("#result").text("Logged in as " + res.fac_name);
-          console.log("Logged in as " + res.fac_name);
-        }
-      },
-
-      error: function(error){
-        console.log(error);
+      if (res.res_type == "failed") {
+        $("#result").text("Incorrect ID/Password");
+        console.log("Incorrect ID/Password");
       }
 
-    });
-  }
+      else if(res.res_type == "faculty") {
+        $("#result").text("Logged in as " + res.fac_name);
+        console.log("Logged in as " + res.fac_name);
+        localStorage.id = id;
+        location.href = "sentmsg.html";
+      }
+    },
+
+    error: function(error){
+      console.log(error);
+    }
+
+  });
+}
+
+function logout() {
+  localStorage.id = null;
+  location.href = "index.html";
+}
